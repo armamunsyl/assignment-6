5fa325ff61ecf0484dbe02374a5eb6e6ef4e2f87
armamunsyl <armamunsyl@gmail.com>
2025-09-09 02:25:58 +0600

    tree card fetch succesfully

--- index.js
+++ index.js
@@ -9,13 +9,43 @@ const DisplayCategories = (Categories) => {
     const categoriesList = document.getElementById("categories");
     categoriesList.innerHTML = "";
     for (let category of Categories) {
-        console.log(category)
+        // console.log(category)
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-         <button class="w-full text-left px-3 py-2 rounded-lg hover:bg-green-200">${category.category_name}</button>
+         <button onclick="loadTree(${category.id})" class="w-full text-left px-3 py-2 rounded-lg hover:bg-green-200">${category.category_name}</button>
         `;
         categoriesList.append(btnDiv);
     }
 
 }
+const loadTree = (id) => {
+    const url = `https://openapi.programming-hero.com/api/category/${id}`
+    console.log(url)
+    fetch(url)
+        .then((res) => res.json())
+        .then((json) => DisplayTrees(json.plants));
+}
+
+const DisplayTrees = (trees) => {
+    console.log(trees);
+    const loadtreeGrid = document.getElementById("treeGrid");
+    loadtreeGrid.innerHTML = "";
+    for (let tree of trees) {
+        // console.log(tree)
+        const TreeCard = document.createElement("div");
+        TreeCard.innerHTML = `
+        <img src="image"  class="rounded-lg">
+      <h3 class="mt-2 font-semibold text-sm">Mamun</h3>
+      <p class="text-xs text-slate-600">mia</p>
+      <span class="text-sm font-semibold mt-1">৳100</span>
+      <span class="inline-block mt-1 px-2 py-1 text-xs bg-green-100 text-green-700 rounded">nai</span>
+      <button class="mt-auto bg-green-600 text-white rounded-lg py-2 text-sm font-semibold hover:bg-green-700">
+        Add to Cart
+      </button>
+        `;
+        loadtreeGrid.append(TreeCard);
+    }
+
+}
+
 LoadCatagories();
